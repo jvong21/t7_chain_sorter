07f0edabd8dfb14fd2d76349aac3a92be5be5768
Justin Vongpanya <jj.vongp@gmail.com>
2018-12-13 11:14:53 -0700

    Added sample frame data files. Updated sample of chain_file_creator

--- Data/use_chain_file_creator.rb
+++ Data/use_chain_file_creator.rb
@@ -3,4 +3,4 @@ require_relative 'ChainFileCreator/chain_file_creator'
 
 chain_file_creator = Chain_File_Creator.new
 
-chain_file_creator.create_chain_file("D:\\JJ\\Downloads\\Alisa Frame Data T7FR.xlsx", "D:\\JJ\\Downloads\\Alisa.txt")
+chain_file_creator.create_chain_file("../FrameData/Alisa Frame Data T7FR.xlsx", "../FrameData/Alisa.txt")
